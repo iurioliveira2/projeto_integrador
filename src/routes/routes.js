a06aa264f79cf9ceae61da452b8d229bec555a9e
Iuri <iuri@gmail.com>
2024-12-02 19:04:25 -0300

    update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const UsuarioController = require('../controllers/usuario.controller');
+const SolicitacoesController = require('../controllers/solicitacoes.controller');
+const VoluntariosController = require('../controllers/voluntarios.controller');
+const EventosController = require('../controllers/eventos.controller');
+const EmpresasController = require('../controllers/empresas.controller');
 
 const router = express.Router();
 
-// Definir as rotas
+//login
+router.post('/login', UsuarioController.login);
+
+// usuario
 router.post('/usuarios', UsuarioController.createUsuario);
 router.get('/usuarios', UsuarioController.getUsuarios);
 router.get('/usuarios/:id', UsuarioController.getUsuarioById);
 router.put('/usuarios/:id', UsuarioController.updateUsuario);
 router.delete('/usuarios/:id', UsuarioController.deleteUsuario);
+
+//solicitaçoes
+router.post('/solicitacoes', SolicitacoesController.createSolicitacao);
+router.get('/solicitacoes', SolicitacoesController.getSolicitacoes);
+router.get('/solicitacoes/:id', SolicitacoesController.getSolicitacaoById);
+router.put('/solicitacoes/:id', SolicitacoesController.updateSolicitacao);
+router.delete('/solicitacoes/:id', SolicitacoesController.deleteSolicitacao);
+
+//voluntarios
+router.post('/voluntarios', VoluntariosController.createVoluntario);
+router.get('/voluntarios', VoluntariosController.getVoluntarios);
+router.get('/voluntarios/:id', VoluntariosController.getVoluntarioById);
+router.put('/voluntarios/:id', VoluntariosController.updateVoluntario);
+router.delete('/voluntarios/:id', VoluntariosController.deleteVoluntario);
+
+//empresas
+router.post('/empresas', EmpresasController.createEmpresa);
+router.get('/empresas', EmpresasController.getEmpresas);
+router.get('/empresas/:id', EmpresasController.getEmpresaById);
+router.put('/empresas/:id', EmpresasController.updateEmpresa);
+router.delete('/empresas/:id', EmpresasController.deleteEmpresa);
+
+//eventos
+router.post('/eventos', EventosController.createEvento);
 
 module.exports = router;
